699210184291c4c4ebd8228c31f6a5adeaec9d8e
Raghad <raghadmtaleb@gmail.com>
2020-02-21 17:32:09 +0300

    added code lol

--- 0202_Happy_Number.py
+++ 0202_Happy_Number.py
@@ -1,1 +1,23 @@
-202. Happy Number
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        seen = set()
+        # n=19
+        found=False
+        j=0
+        while not found:
+            lis=[]
+            # print
+            for i in str(n):
+                lis.append(int(i)**2)
+                # print(lis)
+            if sum(lis)==1:
+                return True
+            else:
+                prev_len= (len(seen))
+                seen.add(sum(lis))
+                if len(seen)==prev_len:
+                    return False
+                n= str(sum(lis))
+                # print(n)
+                lis = []
+
