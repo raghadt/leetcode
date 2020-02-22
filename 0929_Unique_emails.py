2b030837736dbf402e8fa8f1ff71ee5272bddcf5
Raghad <raghadmtaleb@gmail.com>
2020-02-22 09:53:02 +0300

    adding compl.

--- 0929_Unique_emails.py
+++ 0929_Unique_emails.py
@@ -1,5 +1,6 @@
 # Unique Emails
-
+# --- Time Complexity: O(C), where C is the total content of emails. 
+# --- Space Complexity: O(C). 
 email_set = set()
 for email in emails:
     local, domain = email.split('@')
